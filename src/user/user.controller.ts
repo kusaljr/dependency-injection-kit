c5f037a9d50e3d4dc05fe44f4c403c6fa5cfa22d
Kusal Lamsal <prastutlamsal41@gmail.com>
2025-05-30 10:47:53 +0545

    fix paths

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req } from "../../lib/decorators/express";
-import { RateLimit } from "../../lib/ops/rate-limit/rate-limit";
-import { React } from "../../lib/ops/react/decorator";
+import { Controller, Get, Req } from "@express-di-kit/decorators/express";
+import { CircuitBreaker } from "@express-di-kit/ops/circuit-breaker/circuit-breaker";
+import { RateLimit } from "@express-di-kit/ops/rate-limit/rate-limit";
+import { React } from "@express-di-kit/ops/react/decorator";
 import { IsAuthenticated } from "./jwt-middleware";
 import { UserService } from "./user.service";
 
@@ -10,17 +11,27 @@ import { UserService } from "./user.service";
   windowMs: 30 * 1000,
   errorMessage: "Too many requests, please try again later.",
 })
+@IsAuthenticated()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get("/profile")
+  @CircuitBreaker({
+    cooldownTime: 60 * 1000,
+    enableLogging: true,
+    failureThreshold: 0.5,
+    fallbackFn() {
+      return Promise.resolve({
+        error: "Service is currently unavailable, please try again later.",
+      });
+    },
+  })
   getProfile() {
     return this.userService.getUserProfile();
   }
 
   @Get("/list")
   @React()
-  @IsAuthenticated()
   getUserList(@Req req: Request & { user: { id: number; name: string } }) {
     return {
       data: {
